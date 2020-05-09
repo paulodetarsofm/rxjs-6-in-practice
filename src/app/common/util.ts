49f59d3ef489f38a2a94347606dc5da9eb1f486c
Paulo de Tarso Furtado Machado <paulodetarsofm@yahoo.com.br>
2020-05-09 17:26:34 +0100

    Implementing a Cancellable HTTP Observable

--- src/app/common/util.ts
+++ src/app/common/util.ts
@@ -2,9 +2,14 @@ import { Observable, Subscriber } from 'rxjs';
 
 export function createHttpObservable(url: string): Observable<any> {
   return new Observable((observer: Subscriber<any>) => {
-    fetch(url)
+
+    // Allows the cancellation of the fetch request
+    const controller = new AbortController();
+    const signal = controller.signal;
+
+    fetch(url, { signal })
       .then((response: Response) => {
-        return response.json() || undefined;
+        return response.json();
       })
       .then((body: any) => {
         observer.next(body);
@@ -13,5 +18,7 @@ export function createHttpObservable(url: string): Observable<any> {
       .catch((error) => {
         observer.error(error);
       });
+
+    return () => controller.abort();
   });
 }
